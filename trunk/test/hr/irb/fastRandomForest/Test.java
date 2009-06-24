8cb40427f75df110ed7a82a88b1aee598016b52e
sdvillal <sdvillal@1917d674-7e68-11dd-90a9-6306d3ffb32f>
2009-06-24 10:50:20 +0000

    will rework a bit the tests added weka and junit jars

--- trunk/test/hr/irb/fastRandomForest/Test.java
+++ trunk/test/hr/irb/fastRandomForest/Test.java
@@ -13,21 +13,37 @@ import hr.irb.fastRandomForest.data.TestDatasets;
 import weka.core.Instances;
 
 /**
+ * Some tests.
+ *
  * @author Santi Villalba
  * @version $Id$
  */
 public class Test{
 
-  public static void main(String ... args) throws Exception{
+  private static void checkMostImportantIsMostImportant(double[] importances, int mostImportante, Instances data){
+    double mostImportantImportance = importances[mostImportante];
+    for(int i = 0; i < importances.length; i++)
+      if(mostImportantImportance < importances[i])
+        System.err.println("This is wrong, " +data.attribute(i).name() + ", " + importances[i]);
+  }
 
-    Instances data = TestDatasets.vote();
+  private static double[] importances(FastRandomForest frf, Instances data) throws Exception{
+    frf.buildClassifier(data);
+    return frf.getFeatureImportances();
+  }
+
+  public static void main(String... args) throws Exception{
+
+    //Our forest
     FastRandomForest rf = new FastRandomForest();
     rf.setNumTrees(100);
-    double tic = System.nanoTime();
-    rf.buildClassifier(data);
-    System.err.println("Nanos: " +(System.nanoTime() - tic));
-    double[] featureImportances = rf.getFeatureImportances();
-    for(int i = 0; i < featureImportances.length; i++)
-      System.err.println(data.attribute(i).name() +", " +featureImportances[i]);
+
+    //Breiman's paper, vote
+    Instances data = TestDatasets.vote();
+    checkMostImportantIsMostImportant(importances(rf, data), 4, data);
+
+    //Breiman's paper, diabetes
+    data = TestDatasets.diabetes();
+    checkMostImportantIsMostImportant(importances(rf, data), 1, data);
   }
 }
